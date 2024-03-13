136dadbcdc5f54a96f02113cad1c7236a0b98cc0
Kirill Bykov <bykov_kiryusha@internet.ru>
2024-03-13 16:38:46 +0300

    Окно входа, firebase

--- app/src/main/java/com/example/gpstracker/LoginActivity.java
+++ app/src/main/java/com/example/gpstracker/LoginActivity.java
@@ -1,44 +1,24 @@
 package com.example.gpstracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
-    Button btnlogin;
-    DB myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
-        username = (EditText) findViewById(R.id.user_name1);
-        password = (EditText) findViewById(R.id.password1);
-        btnlogin = (Button) findViewById(R.id.btn_signin1);
-        myDB = new DB(this);
-
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-
-                if (user.equals("") || pass.equals("")) Toast.makeText(LoginActivity.this, "Заполните все поля", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkuser = myDB.checkuserpassword(user, pass);
-                    if (checkuser == true) Toast.makeText(LoginActivity.this, "Вы успешно вошли", Toast.LENGTH_SHORT).show();
-                    else Toast.makeText(LoginActivity.this, "Неверные учётные данные", Toast.LENGTH_SHORT).show();
-                }
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_log), (v, insets) -> {
+        //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        //    return insets;
+        //});
     }
 }
