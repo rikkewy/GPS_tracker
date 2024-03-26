773699d2a07c1c014e4eb99e87fc47426ea6bfad
user <anastasiavoronina334@gmail.com>
2024-03-27 01:36:32 +0300

    navigation bar+дописала фрагменты,но с ошибками

--- app/src/main/java/com/example/gpstracker/Main.java
+++ app/src/main/java/com/example/gpstracker/Main.java
@@ -1,18 +1,61 @@
 package com.example.gpstracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Main extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class Main extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.training);
     }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public  boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.person:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment)
+                        .commit();
+                return true;
+
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment)
+                        .commit();
+                return true;
+
+            case R.id.training:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+
+
 }
