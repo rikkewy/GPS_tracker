44fcae490e0e96963c038cdbf0f27f1a1542492d
Kirill Bykov <bykov_kiryusha@internet.ru>
2024-03-29 19:19:09 +0300

    Диалоговое окно с результатами тернировки(не пока макет), кнопка СТОП в акстивности тренировки

---
+++ app/src/main/java/com/example/gpstracker/DialogAftTraining.java
@@ -0,0 +1,30 @@
+package com.example.gpstracker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogAftTraining extends DialogFragment {
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String str = getArguments().getString("key");
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setTitle("Тренировка завершена!")
+                .setMessage(str)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Закрываем диалоговое окно
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
