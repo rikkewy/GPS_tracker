ef51491e55a95b8ac53e25bcfafa704969c34302
Kirill Bykov <bykov_kiryusha@internet.ru>
2024-05-28 09:50:32 +0300

    Пока ничего не работает, просто коммит

--- app/src/main/java/com/example/gpstracker/Health.java
+++ app/src/main/java/com/example/gpstracker/Health.java
@@ -1,13 +1,22 @@
 package com.example.gpstracker;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,14 +24,11 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class Health extends AppCompatActivity {
-    private TextView massa;
-    private TextView proportion;
-    private TextView weight;
-    private TextView strenght;
-    private Button btn_massa;
-    private Button btn_proportion;
-    private Button btn_weight;
-    private Button btn_strenght;
+    private TextView rost;
+    private TextView wes;
+    private TextView long_of_step;
+    private TextView gender;
+    private TextView circ_of_chest;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -30,45 +36,66 @@ public class Health extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_health);
-        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //  v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //  return insets;
-        // });
-        massa = findViewById(R.id.massa);
-        proportion = findViewById(R.id.proportion);
-        weight = findViewById(R.id.weight);
-        strenght = findViewById(R.id.strength);
-        btn_massa = findViewById(R.id.btn_massa);
-        btn_proportion = findViewById(R.id.btn_proportion);
-        btn_weight = findViewById(R.id.btn_weight);
-        btn_strenght = findViewById(R.id.btn_strength);
-        btn_massa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent switcher = new Intent(Health.this, IndexMassa.class);
-                Health.this.startActivity(switcher);
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-        btn_proportion.setOnClickListener(new View.OnClickListener() {
+
+        rost = findViewById(R.id.your_height);
+        wes = findViewById(R.id.your_weight);
+        long_of_step = findViewById(R.id.your_step);
+        gender = findViewById(R.id.your_gender);
+        circ_of_chest = findViewById(R.id.your_chest);
+
+        rost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent switcher = new Intent(Health.this, IndexPropartion.class);
-                Health.this.startActivity(switcher);
-            }
-        });
-        btn_weight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent switcher = new Intent(Health.this, Index_Weight_height.class);
-                Health.this.startActivity(switcher);
-            }
-        });
-        btn_strenght.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent switcher = new Intent(Health.this, Index_strenght.class);
-                Health.this.startActivity(switcher);
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(Health.this);
+                builderSingle.setTitle("Select One Name:-");
+
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(Health.this, android.R.layout.select_dialog_singlechoice);
+                arrayAdapter.add("Hardik");
+                arrayAdapter.add("Archit");
+                arrayAdapter.add("Jignesh");
+                arrayAdapter.add("Umang");
+                arrayAdapter.add("Gatti");
+                arrayAdapter.add("hdhdhd");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+                arrayAdapter.add("dff");
+
+                builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = arrayAdapter.getItem(which);
+                        AlertDialog.Builder builderInner = new AlertDialog.Builder(Health.this);
+                        builderInner.setMessage(strName);
+                        builderInner.setTitle("Your Selected Item is");
+                        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builderInner.show();
+                    }
+                });
+                builderSingle.show();
+
             }
         });
     }
